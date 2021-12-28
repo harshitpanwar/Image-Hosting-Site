2f24c7508cfd705bc13e48ebde3a3a6da03660f4
Anubhav Sahu <anubhavsahu71@gmail.com>
2021-12-28 21:28:21 +0530

    JSX Structure

---
+++ Mern/Mern_Memories_App/client/src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <h1>
+            Upload your Pictures here...
+        </h1>
+    );
+}
+
+export default Form;
---
+++ Mern/Mern_Memories_App/client/src/components/Posts/Post/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <h1>
+            The uploaded pictures is shown here...
+        </h1>
+    )
+}
+
+export default Post
