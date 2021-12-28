4345c2d4675b5d0fe51f54e6a1bdbd56fc8efbbe
Anubhav Sahu <anubhavsahu71@gmail.com>
2021-12-28 21:27:58 +0530

    JSX Structure

--- Mern/Mern_Memories_App/client/src/App.js
+++ Mern/Mern_Memories_App/client/src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid, Icon } from '@material-ui/core'
+
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import icon from './images/icon.png'
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+      <Container maxwidth="lg">
+          <AppBar position="static" color="inherit">
+              <Typography variant="h1" align="center">Image Hosting Site</Typography>
+              <img src={icon} alt="image hosting icon" height="70px" width="70px"/>
+          </AppBar>
+        <Grow in>
+            <Container>
+                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+      </Container>  
     );
 }
 
