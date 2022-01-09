1b1896a91370b3da415fa1dd870758cc4d5d6a19
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-01-09 15:50:38 +0530

    made upload page in Form.js

--- Mern/Mern_Memories_App/client/src/components/Form/Form.js
+++ Mern/Mern_Memories_App/client/src/components/Form/Form.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import useStyles from './styles'
+import './Form.css';
 
 const Form = () => {
     const classes = useStyles();
     return (
-        <h1 className={classes.Form}>
+        <div className={classes.div}>
+            <h1 className={classes.form}>
             Upload your Pictures here...
-        </h1>
+            </h1>
+
+            <div className={classes.Input}>
+                <a>Enter Username</a>
+                <div class="boxdiv"><input className={classes.box}></input></div>
+
+                <a>Enter Title of the post</a>
+                <div class="boxdiv"><input className={classes.box}></input></div>
+
+                <a>Enter Description of the post</a>
+                <div class="boxdiv"><input className={classes.box}></input></div>
+
+                <a>Enter Tags</a>
+                <div class="boxdiv"><input className={classes.box}></input></div>
+
+                <div class="upload-button">
+                    <button class="uploadbutton">Upload a file</button>
+                    <input type="file" name="file"/>
+                </div>
+            </div>
+        </div>
     );
 }
 
